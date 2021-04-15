479f197efc0ab8e943ec0efa29f4babe809517a7
User-P <pether.1994@gmail.com>
2021-04-14 22:44:09 -0500

    fix: index competences

--- app/Http/Livewire/CompetencesIndex.php
+++ app/Http/Livewire/CompetencesIndex.php
@@ -22,11 +22,7 @@ class CompetencesIndex extends Component
         $this->subcategory = Subcategory::find($this->subcategory_id);
         $competences = Competence::whereDate('end_date', '>=', Carbon::today()->toDateString())
             ->status(Competence::PUBLICADO)
-            ->subcategory($this->subcategory_id);
-        if (auth()->user()) {
-            $competences = $competences->level(auth()->user()->level_id);
-        }
-        $competences = $competences
+            ->subcategory($this->subcategory_id)
             ->latest('id')->paginate(8);
         return view('livewire.competences-index', compact('competences', 'subcategories'));
     }
