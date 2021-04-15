2096d0d21c453e6a6e64ba7e698a401f7192b5fe
Irving <irvingrodrigues11@gmail.com>
2021-04-14 21:41:06 -0600

    card

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,51 +1,39 @@
 @props(['course'])
 
-<div class="container mx-auto bg-white rounded-xl">
-        <div clas="relative pb-48 mt-5 mb-4">
-            <img class="h-41 sm:h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
+<article class="card flex flex-col ">
+    <img class="h-41 sm:h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
+    <div class="card-body flex-1 flex flex-col ">
+        <h1 class="text-2xl mb-2 text-center"> {{$course->name}}</h1>
+        <p class="text-md"> {!!Str::limit($course->description,50)!!}</p>
+        <p class="text-gray-500 text-sm mb-2 mt-auto">Prof. {{$course->teacher->name}}</p>
+        <div class="flex">
+            @if ($course->rating!==6) 
+            <ul class="flex text-sm">
+                <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 1 ? 'yellow' : 'gray'}}-400"></i>
+                </li>
+                <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 2 ? 'yellow' : 'gray'}}-400"></i>
+                </li>
+                <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 3 ? 'yellow' : 'gray'}}-400"></i>
+                </li>
+                <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 4 ? 'yellow' : 'gray'}}-400"></i>
+                </li>
+                <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 5 ? 'yellow' : 'gray'}}-400"></i>
+                </li>
+            </ul>
+            <span class="text-gray-700">{{$course->rating}}</span>
+            @endif
+            <p class="text-sm text-gray-500 ml-auto">
+                <i class="fas fa-users"></i>
+                {{($course->students_count)}}</p>
         </div>
-    <div class="flex flex-wrap mx-4">
-        <div class="flex flex-wrap mx-auto">
-            <h1 class="p-4 text-2xl mb-4 text-center font-bold"> {{$course->name}}</h1>
-            <p class="text-md"> {!!Str::limit($course->description,50)!!}</p><br>
-            <p class="mt-2 mb-2  font-bold">Prof. {{$course->teacher->name}}</p><br>
-            <div class="p-4 border-t flex mx-auto text-sm text-gray-600">
-                @if ($course->rating!==6)
-                    <ul class="flex text-sm"> 
-                        <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 1 ? 'yellow' : 'gray'}}-400"></i>
-                        </li>
-                        <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 2 ? 'yellow' : 'gray'}}-400"></i>
-                        </li>
-                        <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 3 ? 'yellow' : 'gray'}}-400"></i>
-                        </li>
-                        <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 4 ? 'yellow' : 'gray'}}-400"></i>
-                        </li>
-                        <li class="mr-1"><i class="fas fa-star text-{{$course->rating>= 5 ? 'yellow' : 'gray'}}-400"></i>
-                        </li>
-                    </ul>
-                <span class="text-gray-700">{{$course->rating}}</span>
-                @endif
-
-            </div>
-            <div class="p-4 border-t flex mx-auto text-sm text-gray-600">
-                <span class="text-gray-700"> 
-                    <i class="fas fa-users"></i>
-                        {{($course->students_count)}}
-                    </span>
-                </div>
-            <div class="p-4 border-t  border-b mt-3 flex mx-auto ">
-                <span class="text-sm font-semibold">
-                    @if ($course->price==0)
-                    <span class="font-bold text-xl "> Gratis</span>
-                    @else
-                    <p class="font-bold text-2xl "> $ {{$course->price}} MXN</p>
-                    @endif
-                </span>
-            </div>
-            <a href="{{route('course.show',$course)}}" 
-             class="block text-center w-full mt-4 bg-pink-600 text-white mb-4 font-bold py-2 mx-auto rounded-xl ">
-                Más información
-            </a>
-        </div>
+        @if ($course->price==0)
+        <p class="my-2 text-gray-400 font-bold"> Gratis</p>
+        @else
+        <p class="text-2xl mx-auto my-2 text-gray-500 font-bold"> $ {{$course->price}} MXN</p>
+        @endif
+        <a href="{{route('course.show',$course)}}" 
+        class="bg-gradient-to-r from-purple-400 to-pink-400 rounded text-white text-center text-xl my-2  ">
+            Más información
+        </a>
     </div>
-</div>
+</article>
