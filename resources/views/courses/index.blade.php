6e8ee88dd9c893bacdfb2810b56dd55d747f347e
Irving Rodriguez <irvingrodrigues11@gmail.com>
2021-06-11 23:04:21 -0500

    slider cursos remastered

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,122 +1,56 @@
 <x-app-layout>
-    @section('header')
-    <!--Aqui empieza el Header --->
-    
-    <script>
-        var cont=0;
-        function loopSlider(){
-            var xx= setInterval(function(){
-            switch(cont)
-            {
-            case 0:{
-                $("#slider-1").fadeOut(400);
-                $("#slider-2").delay(400).fadeIn(400);
-                $("#sButton1").removeClass("bg-purple-600");
-                $("#sButton2").addClass("bg-purple-600");
-            cont=1;
-        
-            break;
-            }
-            case 1:
-            {
-        
-                $("#slider-2").fadeOut(400);
-                $("#slider-1").delay(400).fadeIn(400);
-                $("#sButton2").removeClass("bg-purple-600");
-                $("#sButton1").addClass("bg-purple-600");
-           
-            cont=0;
-                
-            break;
-            }
-            case 2:
-            {
-        
-                $("#slider-3").fadeOut(400);
-                $("#slider-2").delay(400).fadeIn(400);
-                $("#sButton3").removeClass("bg-purple-600");
-                $("#sButton2").addClass("bg-purple-600");
-           
-            cont=2;
-                
-            break;
-            }
-            }},8000);
 
-        }
+@section('header')
+<div class="bg-purple-800">
+    <div class="slideshow">
 
-        function reinitLoop(time){
-        clearInterval(xx);
-        setTimeout(loopSlider(),time);
-        }
-
-
-
-        function sliderButton1(){
-
-        $("#slider-2").fadeOut(400);
-        $("#slider-1").delay(400).fadeIn(400);
-        $("#sButton2").removeClass("bg-purple-600");
-        $("#sButton1").addClass("bg-purple-600");
-        reinitLoop(4000);
-        cont=0
-
-        }
-
-        function sliderButton2(){
-        $("#slider-1").fadeOut(400);
-        $("#slider-2").delay(400).fadeIn(400);
-        $("#sButton1").removeClass("bg-purple-600");
-        $("#sButton2").addClass("bg-purple-600");
-        reinitLoop(4000);
-        cont=1
-        
-        }
-
-        function sliderButton3(){
-        $("#slider-2").fadeOut(400);
-        $("#slider-3").delay(400).fadeIn(400);
-        $("#sButton2").removeClass("bg-purple-600");
-        $("#sButton3").addClass("bg-purple-600");
-        reinitLoop(4000);
-        cont=2
-        
-        }
-
-
-        $(window).ready(function(){
-            $("#slider-2").hide();
-            $("#sButton1").addClass("bg-purple-600");
-
-
-            loopSlider();
-        
-
-        
-        
-        
-        
-        });
-    </script>
-    <div class=" bg-purple-800 pb-5">
-        <div id="slider-1">
-            <div class="relative top-0 w-full  mb-2  bg-cover bg-center opacity-75 text-white py-72 px-10 h-screen w-screen pb-5"
-                style='background-image: url("img/slider-yohana.jpg");'>
-            </div>
+        <div class="mySlides fade opacity-75">
+          <img src="{{asset('img/slider-renato.png')}}" style="width:100%">
         </div>
-        <div id="slider-2" >
-            <div class="relative top-0 w-full bg-cover bg-auto bg-center  opacity-75 text-white py-72 px-10 h-screen w-screen pb-5"
-                style='background-image: url("img/slider-renato.png");'>
-            </div>
+
+        <div class="mySlides fade opacity-75">
+          <img src="{{asset('img/slider-yohana.jpg')}}" style="width:100%">
         </div>
+
+        <div class="mySlides fade opacity-75">
+          <img src="{{asset('img/slider-aron.png')}}" style="width:100%">
+        </div>
+        <div class="relative flex justify-evenly pb-2 mx-auto mt-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold  rounded-full " onclick="plusSlides(-1)"><i class="far fa-arrow-alt-circle-left fa-2x"></i></button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold  rounded-full " onclick="plusSlides(1)"><i class="far fa-arrow-alt-circle-right fa-2x"></i></button>
+        </div>
+
     </div>
-    <div class="bg-purple-800 ">
-        <div class="flex justify-between w-12 mx-auto pb-2">
-            <button id="sButton1" onclick="sliderButton1()" class="bg-white rounded-full w-4 p-2 mr-4 "></button>
-            <button id="sButton2" onclick="sliderButton2()" class="bg-white rounded-full w-4 p-2 mr-4 "></button>
-            <!--<button id="sButton3" onclick="sliderButton3()" class="bg-white rounded-full w-4 p-2 mr-4"></button>-->
-        </div>
-    </div>
+</div>
+
+<script>
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
+</script>
     @endsection
     <div class="mx-auto bg-purple-800">
         <h1 class="text-6xl text-white font-bold mx-auto text-center py-10">Nuevos Cursos</h1>
