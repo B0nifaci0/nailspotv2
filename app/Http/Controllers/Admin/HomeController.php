dbc33c920fe3e17126071eb30d229c484c6d3e03
DuarteDc <al221910409@gmail.com>
2021-08-16 11:07:44 -0500

    Index Dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,81 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Competence;
+use App\Models\Course;
+use App\Models\User;
+use App\Models\Sale;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $totalCourse=0;
+        $totalInstructor=0;
+        $totalCompetence=0;
+        $student=0;
+        $nameCourse=[];
+        $valorCourse=[];
+        $users=[];
+        $judge=0;
+        $allCompetences=0;
+        $users=User::all();
+        foreach ($users as $key => $user) {
+            if($user->hasRole(1) || $user->hasRole(2)){
+                $totalInstructor++;
+            }
+        }     
+        foreach ($users as $key => $user) {
+            if($user->hasRole(3)){
+                $judge++;
+            }
+        }
+        $competences=Competence::all();
+        foreach ($competences as $key => $competence) {
+            $totalCompetence+=$competence->total;
+            $allCompetences=$competence->whereStatus(2)->get();
+        }
+        $courses=Course::whereStatus(3)->get();
+        foreach ($courses as $key => $course) {
+            $totalCourse+=$course->total;
+            $student+=$course->students->count();
+        }   
+        $coursesAll=DB::table('courses')->select('courses.name', DB::raw('count(sales.saleable_id) as total'))
+        ->where('courses.status','=',3)
+        ->groupBy('courses.name')
+        ->orderBy('total', 'DESC')
+        ->take(10)
+        ->join('sales', function ($join) {
+            $join->on('courses.id', '=', 'sales.saleable_id')
+            ->where('sales.saleable_type', '=', 'App\\Models\\Course');
+        })->get();
+        foreach ($coursesAll as $key => $course) {
+            $nameCourse[]=$course->name;
+            $valorCourse[]=$course->total;
+        }
+        $salesMonth=DB::select("SELECT  MONTHNAME(sales.`created_at`) AS mes,SUM(sales.`final_price`) AS total
+        FROM sales WHERE YEAR(sales.`created_at`) = YEAR(CURDATE())
+        GROUP BY mes ORDER BY (
+                CASE WHEN mes='January' THEN 1 
+                WHEN mes='February' THEN 2 
+                WHEN mes='March' THEN 3 
+                WHEN mes='April' THEN 4 
+                WHEN mes='May' THEN 5 
+                WHEN mes='June' THEN 6 
+                WHEN mes='July' THEN 7 
+                WHEN mes='August' THEN 8 
+                WHEN mes='September' THEN 9 
+                WHEN mes='October' THEN 10     
+                WHEN mes='November' THEN 11    
+                WHEN mes='December' THEN 12    
+                ELSE 0 END 
+            );
+       ");
+        foreach ($salesMonth as $key => $totalSales) {
+            $date[]=$totalSales->mes;
+            $sales[]=$totalSales->total;
+        }
+        return view('admin.index', compact('sales', 'date','student', 'courses', 'totalCourse', 'totalCompetence', 'nameCourse','valorCourse','totalInstructor', 'allCompetences', 'judge'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,20 +3,188 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-<h1>Inicio</h1>
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>{{$student}}</h3>
+                <p>Alumnos inscritos</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-users"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>{{$courses->count()}}</h3>
+                <p>Cursos Publicados</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-book"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-light">
+            <div class="inner">
+                <h3>{{$totalInstructor}}</h3>
+                <p>Instructores</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-user-circle"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>${{$totalCourse}}</h3>
+                <p>Cursos Vendidos</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-credit-card"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-primary">
+            <div class="inner">
+                <h3>${{$totalCompetence}}</h3>
+                <p>Competencias Vendidas</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-credit-card"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>{{$allCompetences->count()}}</h3>
+                <p>Competencias Publicadas</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-puzzle-piece"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-dark">
+            <div class="inner">
+                <h3>{{$judge}}</h3>
+                <p>Jueces</p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-university"></i>
+            </div>
+        </div>
+    </div>
+</div>
+
 @stop
 
 @section('content')
-<p>Bienvenido al panel de administración.</p>
+@if ($valorCourse)
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-2 col-md-6 bg-white rounded pb-4">
+            <h3 class="text-center pt-4">Cursos más populares</h3>
+            <canvas id="courses-container"></canvas>
+        </div>
+        <div class="col-sm-2 col-md-6 bg-white rounded pb-4">
+            <h3 class="text-center pt-4">Ventas por mes</h3>
+            <canvas id="sales-container"></canvas>
+        </div>
+    </div>
+    <br><br>
+</div>
+@endif
 @stop
 
 @section('css')
 <link rel="stylesheet" href="/css/admin_custom.css">
-
 @stop
 
 @section('js')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    console.log('Hi!'); 
+    var ctx = document.getElementById('courses-container').getContext('2d');
+    let nameCourse={!!json_encode($nameCourse)!!};
+    let valorCourse={!!json_encode($valorCourse)!!};
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: nameCourse,
+        datasets: [{
+            label: 'Alumnos',
+            data: valorCourse,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 5
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
+</script>
+<script>
+    var ctx = document.getElementById('sales-container').getContext('2d');
+    let months={!!json_encode($date)!!};
+    let sales={!!json_encode($sales)!!};
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: months,
+        datasets: [{
+            label: 'Pesos',
+            data: sales,
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 5
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
 </script>
 @stop
