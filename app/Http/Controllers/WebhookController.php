da18a6955feabec42f4e9043a68b55d3cc7fd39c
Pedro <pether.1994@gmail.com>
2021-09-10 15:00:44 -0500

    feat: course attach oxxo payment

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sale;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class WebhookController extends Controller
@@ -10,8 +11,10 @@ class WebhookController extends Controller
 
     public function __invoke(Request $request)
     {
-        // $sale = Sale::whereStripeId($request->data->object->id);
-        // $sale->status = Sale::APPROVAL;
-        // $sale->save();
+        $sale = Sale::whereStripeId($request->data['object']['id']);
+        $sale->status = Sale::APPROVAL;
+        $sale->save();
+        $course = Course::find($sale->saleable_id);
+        $course->students()->attach($sale->user_id);
     }
 }
